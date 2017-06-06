5ab7ae44ca201f2c8eee9ea01cbbc3da3e806b4d
Tony Mao <tianguohut@163.com>
2017-06-06 18:07:04 +0800

    Updating feature and templates for share feature

--- profiles/juna_v1/themes/junashare_v1/templates/page--product.tpl.php
+++ profiles/juna_v1/themes/junashare_v1/templates/page--product.tpl.php
@@ -137,9 +137,6 @@
           <div class="section row">
             <?php if ($page['highlighted']): ?>
               <div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-            <?php print render($title_prefix); ?>
-            <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-            <?php print render($title_suffix); ?>
             <?php if ($tabs): ?>
               <div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
             <?php print render($page['help']); ?>
@@ -147,6 +144,10 @@
               <ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
             <?php print render($page['content']); ?>
             <?php print $feed_icons; ?>
+            <h3 class="text-center">扫码下载享什么</h3>
+            <div class="text-center">
+              <img class="img-responsive" src="<?php echo url(path_to_theme() . '/images/qr_20170606165100.png', array('absolute' => TRUE)) ?>" alt="">
+            </div>
           </div>
         </div> <!-- /.section, /#content -->
 
